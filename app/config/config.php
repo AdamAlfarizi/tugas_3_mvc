e15b6b662b8dd1091fc9a735897aa6ff649935a7
adam <adamalfal8@mail.unpas.ac.id>
2019-10-23 17:26:45 +0700

    menambah kan bebebra funcsi

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php 
+
+define('BASEURL','http://localhost/tugas_3_mvc/public' );
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'tugas_3_mvc');
--- app/core/Constants.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-define('BASEURL','http://localhost/tugas_3_mvc/public/' );
